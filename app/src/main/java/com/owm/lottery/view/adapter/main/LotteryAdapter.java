d365593b5d8e8d35471defc7fb9775fce02b6920
ouweiming <ouweiming@levoson.cn>
2017-03-30 18:59:41 +0800

    complete lottery code show

---
+++ app/src/main/java/com/owm/lottery/view/adapter/main/LotteryAdapter.java
@@ -0,0 +1,42 @@
+package com.owm.lottery.view.adapter.main;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.owm.lottery.R;
+import com.owm.lottery.model.apiplus.Lottery;
+import com.owm.lottery.model.utils.O;
+
+import java.util.List;
+
+/**
+ * lottery 适配器
+ * Created by ouweiming on 2017/3/30.
+ */
+
+public class LotteryAdapter extends BaseQuickAdapter<Lottery, BaseViewHolder>{
+
+    private int[] numberIds = new int[]{R.id.tv_number1, R.id.tv_number2, R.id.tv_number3,
+            R.id.tv_number4, R.id.tv_number5, R.id.tv_number6, R.id.tv_number7};
+
+    public LotteryAdapter(List<Lottery> data) {
+        super(R.layout.item_lottery, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, Lottery item) {
+        helper.setText(R.id.tv_date, O.subDate(item.getOpentime()));
+        helper.setText(R.id.tv_expect, O.subExpect(item.getExpect()));
+        helper.setText(R.id.tv_week, O.getWeek(item.getOpentime()));
+        helper.setText(R.id.tv_sum, O.getSum(item.getOpencode()));
+        String[] numbers = O.getNumber(item.getOpencode());
+        for (int i = 0, size = numberIds.length/*Math.min(numbers.length, numberIds.length)*/; i < size; i++) {
+            helper.setText(numberIds[i], i < numbers.length ? numbers[i] : "");
+        }
+
+        helper.setVisible(R.id.view_line_top, O.isFour(item.getExpect()));
+        helper.setVisible(R.id.view_line_gap_top, !O.isFour(item.getExpect()));
+
+        helper.setVisible(R.id.view_line_bottom, helper.getAdapterPosition() == getData().size() - 1 && O.isFour(item.getExpect()));
+        helper.setVisible(R.id.view_line_gap_bottom, helper.getAdapterPosition() == getData().size() - 1 && !O.isFour(item.getExpect()));
+    }
+}
--- app/src/main/java/com/owm/lottery/view/main/MainActivity.java
+++ app/src/main/java/com/owm/lottery/view/main/MainActivity.java
@@ -4,17 +4,37 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.google.gson.Gson;
 import com.owm.lottery.R;
+import com.owm.lottery.model.apiplus.Lottery;
+import com.owm.lottery.model.apiplus.Result;
 import com.owm.lottery.presenter.main.IMain;
+import com.owm.lottery.view.adapter.main.LotteryAdapter;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements IMain.MainView{
 
     private IMain.MainPresenter mPresenter;
+
+    private RecyclerView rv_content;
+    private LotteryAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +49,107 @@ public class MainActivity extends AppCompatActivity implements IMain.MainView{
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                getData();
             }
         });
+
+        rv_content = (RecyclerView) findViewById(R.id.rv_content);
+    }
+
+    private void getData() {
+//        getDateXUtils();
+        getDate4HttpURLConnection();
+    }
+
+    private void setAdapter(List<Lottery> data) {
+        if (mAdapter == null) {
+
+            rv_content.setLayoutManager(new LinearLayoutManager(this));
+            rv_content.setAdapter(new LotteryAdapter(data));
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private void getDateXUtils() {
+        //http://f.apiplus.cn/qxc-50.json
+        RequestParams requestParams = new RequestParams("http://f.apiplus.cn/qxc-50.json");
+        x.http().get(requestParams, new Callback.CacheCallback<Result>() {
+            @Override
+            public boolean onCache(Result result) {
+                return false;
+            }
+
+            @Override
+            public void onSuccess(Result result) {
+                setAdapter(result.getData());
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+    }
+
+    private void getDate4HttpURLConnection() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //http://f.apiplus.cn/qxc-50.json
+                HttpURLConnection connection;
+
+                try {
+                    URL url = new URL("http://f.apiplus.cn/qxc-50.json");
+                    connection = (HttpURLConnection) url.openConnection();
+                    // 设置请求方式
+                    connection.setRequestMethod("POST");
+                    // 设置编码格式
+                    connection.setRequestProperty("Charset", "UTF-8");
+                    // 设置超时
+                    connection.setConnectTimeout(10 * 1000);
+
+                    if (connection.getResponseCode() == 200) {
+                        InputStream is = connection.getInputStream();
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        byte[] buffer = new byte[1024 * 8];
+                        int size;
+                        while ((size = is.read(buffer)) >= 1) {
+                            baos.write(buffer, 0 , size);
+                        }
+                        final String result = new String(baos.toByteArray());
+                        Gson gson = new Gson();
+                        final Result result1 = gson.fromJson(result, Result.class);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Collections.reverse(result1.getData());
+                                result1.getData().add(new Lottery());
+                                result1.getData().add(new Lottery());
+                                result1.getData().add(new Lottery());
+                                result1.getData().add(new Lottery());
+                                setAdapter(result1.getData());
+                            }
+                        });
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
     }
 
     @Override
